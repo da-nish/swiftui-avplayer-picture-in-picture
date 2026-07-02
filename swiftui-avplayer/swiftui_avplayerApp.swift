0138b98067b41c6e5c06dd175fb0a2626da83089
danish khan <danishdev64@gmail.com>
2026-07-02 12:43:05 +0530

    Initial Commit

---
+++ swiftui-avplayer/swiftui_avplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_avplayerApp.swift
+//  swiftui-avplayer
+//
+//  Created by PropertyShare on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_avplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
